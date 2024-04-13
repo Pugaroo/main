631b712efb98e359f5a3c756be280d0ca0744ca8
Pugaroo <166765906+Pugaroo@users.noreply.github.com>
2024-04-13 17:06:51 -0400

    idk lol

--- MAIN/main.js
+++ MAIN/main.js
@@ -1,10 +1,3 @@
-const modal = document.querySelector('#modal-id'); // Replace with your modal ID
-const iframe = `<iframe src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>`;
-
-function showModal() {
-    modal.innerHTML = iframe;
-}
-
-function hideModal() {
-    modal.innerHTML = '';
+for (let i = 0; i < 101; i++) {
+    alert("ALERT ALERT!");
 }
